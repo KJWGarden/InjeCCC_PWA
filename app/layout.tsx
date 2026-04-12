86df463b7fd7216e1e203398a074b18526205bab
KJWGarden <mike03131313@gmail.com>
2026-04-13 08:30:47 +0900

    feat:OG 이미지 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,19 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: "채플 출석",
-  description: "인제대학교/가야대학교 채플 출석 관리 시스템",
+  description: "인제대학교/가야대학교 CCC 채플 출석 관리 시스템",
   manifest: "/manifest.json",
+  openGraph: {
+    title: "채플 출석",
+    description: "인제대학교/가야대학교 채플 출석 관리 시스템",
+    images: [{ url: "/ogImage.png", width: 1200, height: 630 }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "채플 출석",
+    description: "인제대학교/가야대학교 CCC 채플 출석 관리 시스템",
+    images: ["/ogImage.png"],
+  },
 };
 
 export const viewport: Viewport = {
